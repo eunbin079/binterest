7f2f9895e822c9e6fd3021b70df8f1578461285f
eunbin079 <eunbin1111j@gmail.com>
2019-11-07 17:27:26 +0900

    home-main add 'setPosition()''addPin()','removePin'

--- pinterest/src/app/module/home/main/main.component.ts
+++ pinterest/src/app/module/home/main/main.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HostListener } from '@angular/core';
-import { $ } from 'protractor';
+
+
 
 @Component({
   selector: 'app-main',
@@ -10,104 +10,182 @@ import { $ } from 'protractor';
 })
 
 export class MainComponent implements OnInit {
-  @HostListener('window:resize',[`$event`])
 
 
-  columNum=6;
+
+  columnNum;
+  startIdx=0;
   cardNum;
   screenWidth;
   list:HTMLDivElement[]=[];
 
   constructor() { 
-    //this.onResize();
-     this.cardNum=30;
+     this.cardNum = 0;
+     this.columnNum = 6;
     
   }
 
   ngOnInit() {
-    this.addPin(0,30);
-  }
-  // onResize(event?){
-  //   this.screenWidth = window.innerWidth;
-  //   this.setList(0,this.cardNum);
-  // }
-  // setList(startIdx,endIdx){
-  //   let i;
-  
-  //   for(i=startIdx;i<endIdx;i++)
-  //   {
-  //     let prevId = (i-this.columNum).toString();
-  //     let prevDoc = document.getElementById(prevId); 
-  //     let nowDoc = document.getElementById(i);
+    this.cardNum=0;
+ 
+    this.addPin3(50);
+ 
+   
 
-  //     let prevHeight:number=+prevDoc.getAttribute("height");
-      
-  //     this.list[i].x=250*(i%this.columNum);
-  //     this.list[i].y=prevHeight+this.list[i-this.columNum].y;
-  //   }
-  // }
-  addPin(startIdx:number,endIdx:number){
+  }
+
+  
+  onResize(event) {
+   
+  // event.target.innerWidth window size 
+   let innerWidth=event.target.innerWidth;
+   let i;
+
+  //Setting Column Number
+   if(innerWidth>1749)
+   {
+     this.columnNum = 6;
+   }
+   else if (innerWidth>1499)
+   {
+     this.columnNum = 5;
+   }
+   else if (innerWidth > 1249)
+   {
+     this.columnNum = 4;
+   }
+   else if (innerWidth > 999)
+   {
+     this.columnNum = 3;
+   }
+   else if (innerWidth >749)
+   {
+     this.columnNum = 2;
+   }
+   else
+   {
+      this.columnNum =1;
+   }
+  
+   // Reposition
+   for(i=this.startIdx;i<this.startIdx+this.cardNum;i++)
+   {
+     let Pin = document.getElementById(i.toString());
+     this.setPosition(Pin);
+   }
+   
+  }
+
+  removePin(num:number){
     let i;
+    let div = document.getElementById("columns");
+
+    for(i=this.startIdx;i<this.startIdx+num;i++)
+    {
+      var Pin = document.getElementById(i.toString());
+
+      console.log(div.removeChild(Pin));
+    }
+
+    this.startIdx += num;
+    this.cardNum -= num;
+  }
+  
+  async addPin3(num:number){
+    
+    let i;
+    let div = document.getElementById("columns");
+    
+    for(i=this.startIdx+this.cardNum;i<this.startIdx+this.cardNum+num;i++){
+      
+       var newPin = document.createElement('div');
+        
+
+        newPin.setAttribute("class","card");
+        newPin.setAttribute("id",i.toString());
+        var srcAddr="../../../../assets/img/8298be61205c0ef9ba51bdb07218be25.jpg"
+        newPin.innerHTML=
+        "<div class=\"imgContainer\">"+
+          "<img class=\"img1\" src=\""+srcAddr+"\"> "+
+        "</div>"+
+        "<div class=\"tagContainer\">"+
+          "<div style=\"width: 100% ;padding-left:8px; padding-right:8px\">"+"</div>"+
+          "<div class=\"tagContent\">"+
+            "<i class=\"fas fa-ellipsis-h\">"+"</i>"+
+          "</div>"+
+        "</div>";
+
+      this.setPosition(newPin);
+      await this.func2(i,newPin,div);
+      console.log(newPin);
+
+    }
+    this.cardNum += num;
+   this.resizeElementHeight(div);
+    
+  }
+  resizeElementHeight(element) {
+    var height = 0;
+    var body = window.document.body;
+    if (window.innerHeight) {
+        height = window.innerHeight;
+    } else if (body.parentElement.clientHeight) {
+        height = body.parentElement.clientHeight;
+    } else if (body && body.clientHeight) {
+        height = body.clientHeight;
+    }
+    element.style.height += ((height - element.offsetTop) + "px");
+  }
+
+  func2(i:number,newPin:HTMLElement,div:HTMLElement) {
+    div.appendChild(newPin);
+
+    return new Promise((resolve,reject)=>{
+      let img = new Image();
+      img.onload = ()=>{
+        newPin.querySelector("img").src=img.src;
+        resolve(img.height);
+      }
+      img.onerror = reject;
+      img.src= "../../../../assets/img/8298be61205c0ef9ba51bdb07218be25.jpg";
+    })
+    
+  }
+
+  setPosition(newPin:HTMLElement){
     let x;
     let y;
-    let div = document.getElementById("columns");
-    setTimeout(() => {
-      for(i=startIdx;i<endIdx;i++){
-        if(i>this.columNum-1){
-        let prevId = (i-this.columNum).toString();
-        console.log("prevId : "+prevId);
-        let prevDoc = document.getElementById(prevId); 
-        console.log("prevDOC : "+prevDoc);
-        let prevCordinateY=prevDoc.getBoundingClientRect();
-        let prevHeight= prevDoc.scrollHeight;
-        console.log(prevCordinateY.top+"height"+prevHeight+"offset"+window.pageYOffset);
-         y=prevCordinateY.top+window.pageYOffset+prevHeight;
-        }
-        else{
-          y=0;
-        }
-  
-        x=250*(i%this.columNum);
-        div.appendChild(this.Pin(i,x,y));
-        console.log("wwwwwwwwwwww"+div);
-        console.log(this.list);
+    
+    let nowPinId = newPin.getAttribute("id");
+    let prevPinId = ( +nowPinId - this.columnNum ).toString();
+    let prevPin = document.getElementById( prevPinId );
+
+    //Setting Cordinate
+
+    x = 250* ( +nowPinId % this.columnNum );
+
+    if (+nowPinId > this.columnNum-1)
+    {
+      let prevCordinateY=prevPin.getBoundingClientRect();
+      let prevHeight= prevPin.scrollHeight;
       
-      }
+      y=prevCordinateY.top+window.pageYOffset+prevHeight;
+    }
+    else
+    {
+      y=0;
+    }
 
-    }, 2000)
-    console.log("ddddddddddd"+document.getElementById("columns"));
+    newPin.style.transform='translateX('+x+'px) translateY('+y+'px)';
+
   }
+
+
   
-  Pin(idx:number,x:number,y:number){
-   
-    var newPin = document.createElement("div");
-
-    newPin.setAttribute("class","card");
-    newPin.setAttribute("id",idx.toString());
-    newPin.style.transform='translateX('+x+'px) translateY('+y+'px)'
-    var srcAddr="../../../../assets/img/8298be61205c0ef9ba51bdb07218be25.jpg"
-    newPin.innerHTML=
-    "<div class=\"imgContainer\">"+
-       "<img class=\"img1\" src=\""+srcAddr+"\"> "+
-    "</div>"+
-    "<div class=\"tagContainer\">"+
-      "<div style=\"width: 100% ;padding-left:8px; padding-right:8px\">"+"</div>"+
-      "<div class=\"tagContent\">"+
-         "<i class=\"fas fa-ellipsis-h\">"+"</i>"+
-      "</div>"+
-    "</div>";
-
-    console.log(document.getElementById(idx.toString()));
-   return newPin;
-
-  }
-
  
   onScroll(){
-    console.log("Scrooolllllll!!");
-    let temp=window.pageYOffset;
-    this.addPin(30,50)
-    let div=document.getElementById("first");
-    console.log("HEIGHT================== :"+div.getAttribute("height"));
-  }
+    console.log("Scrroooll!!");
+    this.removePin(20);
+    this.addPin3(20);
+   }
 }
